bdc34496b91e8a3648b5ad28a01286ad74503c22
afiatamanna06 <afiatamanna06@gmail.com>
2020-07-24 19:25:07 +0600

    app.py updated

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ from tkinter import filedialog
 root = Tk()
 root.geometry("500x600")
 
+def saveFile():
+    print(" ")
+
+button1 = Button(root,text="Save",command=saveFile)
+button1.place(x=10,y=10)
+
 mainloop()
