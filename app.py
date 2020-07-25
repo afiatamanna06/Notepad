65da404b5eeabb322822722a63347406f051749e
afiatamanna06 <afiatamanna06@gmail.com>
2020-07-25 19:23:05 +0600

    app.py updated

--- app.py
+++ app.py
@@ -10,11 +10,17 @@ def saveFile():
     
 def clearFile():
     print(" ")
+    
+def openFile():
+    print(" ")
 
 button1 = Button(root,text="Save",command=saveFile)
 button1.place(x=10,y=10)
 
 button2 = Button(root,text="Clear",command=clearFile)
 button2.place(x=70,y=10)
+
+button3 = Button(root,text="Open",command=openFile)
+button3.place(x=138,y=10)
 
 mainloop()
