3d06c558eef22666752d56b41a15a9fe24aa094e
afiatamanna06 <afiatamanna06@gmail.com>
2020-07-25 19:17:17 +0600

    app.py updated

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ root.geometry("500x600")
 
 def saveFile():
     print(" ")
+    
+def clearFile():
+    print(" ")
 
 button1 = Button(root,text="Save",command=saveFile)
 button1.place(x=10,y=10)
+
+button2 = Button(root,text="Clear",command=clearFile)
+button2.place(x=70,y=10)
 
 mainloop()
